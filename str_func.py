46b05e3ee57673a5c9b631235f407f54b28406bd
SergiusArt <135706262+SergiusArt@users.noreply.github.com>
2023-06-13 00:31:05 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,18 +1,10 @@
 def big_str(s):
     """
-    New describe
     Function that upgrade your text to text with big letter
     """
 
 
     return s.upper()
 
-def first_big_letter(s):
-    """
-    Every word will have first letter is big
-    """
-
-
-    return s.title()
 
 
